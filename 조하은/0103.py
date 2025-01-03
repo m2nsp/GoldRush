be01b4fc3d29b6d22e183ccfb3d6cf78926117bc
haeun228 <137027947+haeun228@users.noreply.github.com>
2025-01-04 02:24:41 +0900

    Create 0103.py

---
+++ 조하은/0103.py
@@ -0,0 +1,18 @@
+# 23회 기출 1번
+import sys
+input = sys.stdin.readline
+
+def solution(P):
+    P.sort()
+    sum = 0
+    temp = 0
+    
+    for i in range(N):
+        temp += P[i]
+        sum += temp
+    
+    return sum
+
+N = int(input())
+P = list(map(int, input().split()))
+print(solution(P))
