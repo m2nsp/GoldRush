c24dd014a70d9c60b661e246f845a3444beab822
m2nsp <129494557+m2nsp@users.noreply.github.com>
2025-01-06 22:05:39 +0900

    Create 0106.py

---
+++ 박민서/0106.py
@@ -0,0 +1,15 @@
+def solution(board, moves):
+    answer = 0      #터진 인형개수
+    depth = len(board)  #인형 뽑기 기계 깊이
+    pick = []       # 뽑힌 인형 리스트
+    for move in moves:
+        for j in range(depth):
+            if board[j][move - 1] != 0:
+                if pick and pick[-1] == board[j][move - 1]:
+                    pick.pop()
+                    answer += 2
+                else:
+                    pick.append(board[j][move - 1])
+                board[j][move - 1] = 0  # 뽑은 인형 제거
+                break
+    return answer
