36825f94490bedd3dae843c8ce0f568eb47876ba
haeun228 <137027947+haeun228@users.noreply.github.com>
2025-01-16 23:53:28 +0900

    Create 0116.py

---
+++ 조하은/0116.py
@@ -0,0 +1,17 @@
+# 2303 쇠막대기
+
+arr = input()
+stack = []
+result = 0
+
+for i in range(len(arr)):
+    if arr[i] == '(':
+        stack.append('(')
+    else:
+        stack.pop()
+        if arr[i-1] == '(':
+            result += len(stack)
+        else:
+            result += 1
+
+print(result)
